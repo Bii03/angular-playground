25ecfd65d4ebf25b7df721fd09202c70d5cb2ceb
Bianca Tesila <tesila@adobe.com>
2017-06-11 17:50:00 +0300

    heroes tutorial with mock service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,30 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule}   from '@angular/forms'; // <-- NgModel lives here
 
-import { AppComponent }  from './app.component';
+import {HeroesComponent}  from './heroes.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard.component';
+import {HeroService} from './hero.service';
+import {AppRoutingModule} from './app-routing.module';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule
+    ],
+    declarations: [AppComponent, HeroesComponent, HeroDetailComponent, DashboardComponent],
+    bootstrap: [AppComponent],
+    providers: [HeroService]
 })
-export class AppModule { }
+export class AppModule {
+}
+
+/*
+ Copyright 2017 Google Inc. All Rights Reserved.
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at http://angular.io/license
+ */
